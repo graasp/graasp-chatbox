929d6494c22e6385ba09c44374ae3a8b5c46b44d
kim <kim.phanhoang@epfl.ch>
2024-08-09 15:13:08 +0200

    refactor: fix

--- cypress/fixtures/members.ts
+++ cypress/fixtures/members.ts
@@ -1,24 +1,16 @@
-import { CompleteMember, MemberType } from '@graasp/sdk';
+import { CompleteMember, MemberFactory } from '@graasp/sdk';
 
 export const MEMBERS: { [key: string]: CompleteMember } = {
-  ANNA: {
+  ANNA: MemberFactory({
     id: '0f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'anna',
     email: 'anna@email.com',
-    type: MemberType.Individual,
-    extra: {},
-    createdAt: '2023-12-11T23:00:45.456Z',
-    updatedAt: '2023-12-11T23:00:45.456Z',
-  },
-  BOB: {
+  }),
+  BOB: MemberFactory({
     id: '1f0a2774-a965-4b97-afb4-bccc3796e060',
     name: 'bob',
     email: 'bob@email.com',
-    type: MemberType.Individual,
-    extra: {},
-    createdAt: '2023-12-11T23:00:45.456Z',
-    updatedAt: '2023-12-11T23:00:45.456Z',
-  },
+  }),
 };
 
 export const CURRENT_MEMBER = MEMBERS.ANNA;
